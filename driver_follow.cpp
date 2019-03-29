9875ace393a969bd1a3d08801835d424cb80d52d
PiRA502 <43596587+PiRA502@users.noreply.github.com>
2019-03-29 12:08:54 +0800

    Follow1.0.3
    //修改了学长代码里一个判断条件下的逻辑错误（“print 2”）
    //转向函数改为cruise里的pid模型
    目前所有头车均能跟完

--- driver_follow.cpp
+++ driver_follow.cpp
@@ -1,39 +1,29 @@
 /***************************************************************************
-
-                 file : driver_cruise.cpp
-    description : user module for CyberFollow
-
+				 file : driver_cruise.cpp
+	description : user module for CyberFollow
  ***************************************************************************/
-
-/*      
-     WARNING !
-     
-	 DO NOT MODIFY CODES BELOW!
-*/
-
+ /*
+	  WARNING !
+	  DO NOT MODIFY CODES BELOW!
+ */
 #ifdef _WIN32
 #include <windows.h>
 #endif
-
 #include "driver_follow.h"
-
-
 static void userDriverGetParam(float LeaderXY[2], float midline[200][2], float yaw, float yawrate, float speed, float acc, float width, int gearbox, float rpm);
 static void userDriverSetParam(float* cmdAcc, float* cmdBrake, float* cmdSteer, int* cmdGear);
 static int InitFuncPt(int index, void *pt);
-
 // Module Entry Point
 extern "C" int driver_follow(tModInfo *modInfo)
 {
-	memset(modInfo, 0, 10*sizeof(tModInfo));
-	modInfo[0].name    = "driver_follow";	// name of the module (short).
-	modInfo[0].desc    =  "user module for CyberFollower" ;	// Description of the module (can be long).
+	memset(modInfo, 0, 10 * sizeof(tModInfo));
+	modInfo[0].name = "driver_follow";	// name of the module (short).
+	modInfo[0].desc = "user module for CyberFollower";	// Description of the module (can be long).
 	modInfo[0].fctInit = InitFuncPt;			// Init function.
-	modInfo[0].gfId    = 0;
-	modInfo[0].index   = 0;
+	modInfo[0].gfId = 0;
+	modInfo[0].index = 0;
 	return 0;
 }
-
 // Module interface initialization.
 static int InitFuncPt(int, void *pt)
 {
@@ -42,28 +32,25 @@ static int InitFuncPt(int, void *pt)
 	itf->userDriverSetParam = userDriverSetParam;
 	return 0;
 }
-
 /*
-     WARNING!
-
+	 WARNING!
 	 DO NOT MODIFY CODES ABOVE!
 */
-
-/* 
-    define your variables here.
-    following are just examples
+/*
+	define your variables here.
+	following are just examples
 */
 static float _midline[200][2];
 static float _yaw, _yawrate, _speed, _acc, _width, _rpm;
 static int _gearbox;
 static float _Leader_X, _Leader_Y;
-const int topGear = 6;		
+const int topGear = 6;
 float distance;
-float expectedDistance;        //most important
+float expectedDistance;  
 float leaderAcc;
 float kAcc = 0.5;
 float leaderSpeed;
-float lastDistance;         
+float lastDistance;        
 float lastLeaderSpeed;      
 float D_err = 0;
 float S_err = 0;
@@ -78,18 +65,13 @@ float kd_s;	//kd for speed							     //
 float kp_d;	//kp for direction						     //
 float ki_d;	//ki for direction					    	 //
 float kd_d;	//kd for direction						     //
-
-
 void updateGear(int *cmdGear);
 double constrain(double lowerBoundary, double upperBoundary, double input);
-
-
-static void userDriverGetParam(float LeaderXY[2], float midline[200][2], float yaw, float yawrate, float speed, float acc, float width, int gearbox, float rpm){
+static void userDriverGetParam(float LeaderXY[2], float midline[200][2], float yaw, float yawrate, float speed, float acc, float width, int gearbox, float rpm) {
 	/* write your own code here */
-	
 	_Leader_X = LeaderXY[0];
 	_Leader_Y = LeaderXY[1];
-	for (int i = 0; i< 200; ++i) _midline[i][0] = midline[i][0], _midline[i][1] = midline[i][1];
+	for (int i = 0; i < 200; ++i) _midline[i][0] = midline[i][0], _midline[i][1] = midline[i][1];
 	_yaw = yaw;
 	_yawrate = yawrate;
 	_speed = speed;
@@ -97,29 +79,26 @@ static void userDriverGetParam(float LeaderXY[2], float midline[200][2], float y
 	_width = width;
 	_rpm = rpm;
 	_gearbox = gearbox;
-
-
 	/* you can modify the print code here to show what you want */
 	//printf("speed %.3f Leader XY(%.3f, %.3f)\n", _speed, _Leader_X, _Leader_Y);
 }
-
 static void userDriverSetParam(float* cmdAcc, float* cmdBrake, float* cmdSteer, int* cmdGear) {
 	/* write your own code here */
 	distance = pow((_Leader_X*_Leader_X + _Leader_Y * _Leader_Y), 0.5);
-	leaderSpeed = _speed + (distance - lastDistance) * 180;   //the speed of leader
+	leaderSpeed = _speed + (distance - lastDistance) * 180;
 	lastDistance = distance;
-	leaderAcc = (leaderSpeed - lastLeaderSpeed) / 0.02;       //the acc of leader
+	leaderAcc = (leaderSpeed - lastLeaderSpeed) / 0.02;
 	lastLeaderSpeed = leaderSpeed;
-	//D_err = distance - expectedDistance;
+	D_err = distance - expectedDistance;
 	S_err = _speed - leaderSpeed;
-	//D_errDiff = (D_err - LastTimeDerr) / 0.02;
-	//D_errSum = 0.2 * D_errSum + D_err;
-	//LastTimeDerr = D_err;
-
+	D_errDiff = (D_err - LastTimeDerr) / 0.02;
+	LastTimeDerr = D_err;
+	kp_d = -1 * (_speed*0.01);
+	ki_d = 0.003*0.02;
+	kd_d = -0.00012;
 	double offset = 0;
 	double threshold = 5;
-
-	//ExpectedDistance (most important)
+	//ExpectedDistance
 	if (_speed < 50)
 	{
 		offset = 0;
@@ -132,11 +111,14 @@ static void userDriverSetParam(float* cmdAcc, float* cmdBrake, float* cmdSteer, 
 	{
 		offset = 0.3 * threshold + 0.7*threshold*(_speed - 150) / 50;
 	}
-	else 
+	else if (_speed < 250)
 	{
 		offset = threshold;
 	}
-	
+	else if (_speed > 200)
+	{
+		offset = threshold;
+	}
 	if (_speed < 80 && leaderAcc>30)
 	{
 		offset = 0.3;
@@ -145,83 +127,83 @@ static void userDriverSetParam(float* cmdAcc, float* cmdBrake, float* cmdSteer, 
 	{
 		offset = 5.7;
 	}
+	expectedDistance = 10.7 + offset;
+	
+	
 
-	expectedDistance = 12+ offset;
-
-
-	/* you can modify the print code here to show what you want */
-
-
-	//speed control
-	if (distance < expectedDistance-0.5  && S_err>0)
+	
+	if (distance < expectedDistance - 0.5 && S_err>0)
 	{
-		printf("  1");
 		*cmdAcc = 0;
 		*cmdBrake = 1;
+		printf(" 1");
 	}
 	else if (distance < expectedDistance && S_err < 2 * (D_err + kAcc * leaderAcc))
 	{
 		*cmdAcc = 0;
-		*cmdBrake = 1;
-		printf("  2");
+		*cmdBrake = 0.2;
+		printf(" 2");
 
 	}
 	else if (distance < expectedDistance && S_err>2 * (D_err + kAcc * leaderAcc))
 	{
 		*cmdAcc = 0;
 		*cmdBrake = S_err / 5;
-		printf("  3");
+		printf(" 3");
 
 	}
-
 	else if (distance > expectedDistance && S_err < 1 * (D_err + kAcc * leaderAcc))
 	{
-		*cmdAcc = 0.8;
+		*cmdAcc = 1;
 		*cmdBrake = 0;
-		printf("  4");
+		printf(" 4");
 
 	}
 	else if (distance > expectedDistance && S_err > 1 * (D_err + kAcc * leaderAcc))
 	{
 		*cmdAcc = 0;
 		*cmdBrake = S_err / 5;
-		printf("  5");
+		printf(" 5");
 
 	}
-
 	if (leaderAcc < -50 && distance < expectedDistance + 0.1)
+	{
 		*cmdBrake = 1;
-
+		printf(" 6");
+	}
 	if (fabs(*cmdSteer) > 0.2 && _speed > 150)
 	{
 		*cmdBrake = S_err / 4.79;
 		*cmdAcc = -1 * S_err;
+		printf(" 7");
+
+	}
+	updateGear(cmdGear);
+	if (_speed < 10)
+	{
+		*cmdGear = 1;
+		printf(" 8");
 	}
 
-	printf("\nspeed %.3f Leader XY(%.3f, %.3f)\tExpectedistance%f", _speed, _Leader_X, _Leader_Y, expectedDistance);
-
-	updateGear(cmdGear);
-	if (_speed < 40)
-		*cmdGear = 1;
-
-	//drection control
-	// 
-
-
 	kp_d = 1;
-	ki_d = 0;
+	ki_d = 0.1;
 	kd_d = 0.5;
 
 	if (_speed < 20)//at the begining (initial)
-		D_err = -atan2(_Leader_X,_Leader_Y	);//original[5]
+		D_err = -atan2(_Leader_X, _Leader_Y);
 	else
 		D_err = 2 * (_yaw - 3 * atan2(_Leader_X, _Leader_Y));
 
 	D_errDiff = D_err - D_errSum;
-	D_errSum = 0.2 * D_errSum + D_err;//ldx: modified
-	lastDistance= D_err;
-	//*cmdSteer = constrain(-1.0, 1.0, kp_d * D_err + ki_d * D_errSum + kd_d * D_errDiff);
-	*cmdSteer = (_yaw - 8 * atan2(_Leader_X, _Leader_Y));
+	D_errSum = 0.2 * D_errSum + D_err;
+
+	*cmdSteer = constrain(-1.0, 1.0, kp_d * D_err + ki_d * D_errSum + kd_d * D_errDiff);
+	//*cmdSteer = (_yaw - 8 * atan2(_Leader_X, _Leader_Y));
+
+
+	/* you can modify the print code here to show what you want */
+	printf(" follow %.3f leader%.3f   XY(%.3f, %.3f)\n", _speed, leaderSpeed, _Leader_X, _Leader_Y);
+
 }
 
 
@@ -315,7 +297,6 @@ void updateGear(int *cmdGear)
 		*cmdGear = 1;
 	}
 }
-
 double constrain(double lowerBoundary, double upperBoundary, double input)
 {
 	if (input > upperBoundary)
