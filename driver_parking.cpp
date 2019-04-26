655d465d717707af51463555785c0710a785465a
henry87653 <henry87653@163.com>
2019-04-26 10:22:49 +0800

    0.0.3
    对学长代码的变量输出

--- driver_parking.cpp
+++ driver_parking.cpp
@@ -3,16 +3,16 @@
 	 All rights reserved
 	 file : driver_parking.cpp
 	 description :对学长代码的变量输出
-	 version: 0.0.1
+	 version: 0.0.3
 	 modified by Lu at  April/26/2019 10:04
 	 https://github.com/henry87653/Engineering-Technological-Innovation-4D
   ***************************************************************************/
 
-/*
-WARNING !
+  /*
+  WARNING !
 
-DO NOT MODIFY CODES BELOW!
-*/
+  DO NOT MODIFY CODES BELOW!
+  */
 
 #ifdef _WIN32
 #include <windows.h>
@@ -81,7 +81,7 @@ static void userDriverGetParam(float lotX, float lotY, float lotAngle,
 	_carX = carX;
 	_carY = carY;
 	_caryaw = caryaw;
-	for (int i = 0; i< 200; ++i) _midline[i][0] = midline[i][0], _midline[i][1] = midline[i][1];
+	for (int i = 0; i < 200; ++i) _midline[i][0] = midline[i][0], _midline[i][1] = midline[i][1];
 	_yaw = yaw;
 	_yawrate = yawrate;
 	_speed = speed;
@@ -96,6 +96,17 @@ static int state = 0;
 static bool backcar = false;
 static bool Stop = false, TurnRight = false, FirstStop = false, startLeftShift = false;
 int ass = 0;
+
+///=================helping functions from TA no need to modify============================
+double constrain(double lowerBoundary, double upperBoundary, double input)
+{
+	if (input > upperBoundary)
+		return upperBoundary;
+	else if (input < lowerBoundary)
+		return lowerBoundary;
+	else
+		return input;
+}
 //???
 void push_back(float arr[], float value) {
 	for (int i = 0; i < 4; i++)
@@ -107,8 +118,9 @@ float getMean(float arr[]) {
 	float sum = 0;
 	for (int i = 4; i >= 0; i = i - 1)
 		sum = sum + arr[i];
-	return sum /5;
+	return sum / 5;
 }
+///=================helping functions from TA no need to modify============================
 static void userDriverSetParam(bool* bFinished, float* cmdAcc, float* cmdBrake, float* cmdSteer, int* cmdGear) {
 	distance = sqrt((_carX - _lotX) * (_carX - _lotX) + (_carY - _lotY) * (_carY - _lotY));
 	haltX = _lotX + 10.8*cos(_lotAngle + 0.15);
@@ -127,10 +139,10 @@ static void userDriverSetParam(bool* bFinished, float* cmdAcc, float* cmdBrake, 
 	if (distance < sqrt(4000.0)) startLeftShift = true;
 	if (midlined < 20) TurnRight = true;
 	if (midlined < 5)  FirstStop = true;
-	if ((fabs(_lotX - X1) < 1 && fabs(_lotY - Y1) < 1) || 
-		(fabs(_lotX - X2) < 1 && fabs(_lotY - Y2) < 1) || 
+	if ((fabs(_lotX - X1) < 1 && fabs(_lotY - Y1) < 1) ||
+		(fabs(_lotX - X2) < 1 && fabs(_lotY - Y2) < 1) ||
 		(fabs(_lotX - X3) < 1 && fabs(_lotY - Y3) < 1) ||
-		(fabs(_lotX - X4) < 1 && fabs(_lotY - Y4) < 1) || 
+		(fabs(_lotX - X4) < 1 && fabs(_lotY - Y4) < 1) ||
 		(fabs(_lotX - X5) < 1 && fabs(_lotY - Y5) < 1))ass = 1;
 	if (!*bFinished) {
 		if (Stop) {
@@ -146,30 +158,30 @@ static void userDriverSetParam(bool* bFinished, float* cmdAcc, float* cmdBrake, 
 			if (fabs(parkdist) > 0.5)k3 = 2;
 			*cmdSteer = -k1 * angle - k2 * avgAngle / 3.14 - 1.404*(parkdist)-1.872*avgPark;
 			if (fabs(_lotX - X1) < 1 && fabs(_lotY - Y1) < 1) {//#1
-				k3 = 4;k2 = 25;
-				*cmdSteer = -k1 * angle - k2 * avgAngle / 3.14 - 1.404*(parkdist)-1.872*avgPark; 
+				k3 = 4; k2 = 25;
+				*cmdSteer = -k1 * angle - k2 * avgAngle / 3.14 - 1.404*(parkdist)-1.872*avgPark;
 			}
 			if (fabs(_lotX - X2) < 1 && fabs(_lotY - Y2) < 1) {//#2 
-				k3 = 4;k2 = 25;
-				*cmdSteer = -k1 * angle - k2 * avgAngle / 3.14 - 1.404*(parkdist)-1.872*avgPark; 
+				k3 = 4; k2 = 25;
+				*cmdSteer = -k1 * angle - k2 * avgAngle / 3.14 - 1.404*(parkdist)-1.872*avgPark;
 			}
 
 			if (fabs(_lotX - X3) < 1 && fabs(_lotY - Y3) < 1) {//#3
-				k3 = 4;k2 = 25;
+				k3 = 4; k2 = 25;
 				*cmdSteer = -k1 * angle - k2 * avgAngle / 3.14 - 1.404*(parkdist)-1.872*avgPark;
 			}
 
 			if (fabs(_lotX - X4) < 1 && fabs(_lotY - Y4) < 1) {//#4 
-				k3 = 4;k2 = 24;
+				k3 = 4; k2 = 24;
 				*cmdSteer = -k1 * angle - k2 * avgAngle / 3.14 - 1.404*(parkdist)-1.872*avgPark;
 			}
 
-			if (fabs(_lotX - X5) < 1 && fabs(_lotY - Y5) < 1){ //#5
+			if (fabs(_lotX - X5) < 1 && fabs(_lotY - Y5) < 1) { //#5
 				k3 = 4; k2 = 25;
 				*cmdSteer = -k1 * angle - k2 * avgAngle / 3.14 - 1.404*(parkdist)-1.872*avgPark;
 			}
-			
-		
+
+
 			if (fabs(_speed) > k3 * distance + 5) {
 				*cmdBrake = 0.2;
 				*cmdGear = -1;
@@ -179,17 +191,17 @@ static void userDriverSetParam(bool* bFinished, float* cmdAcc, float* cmdBrake, 
 				*cmdBrake = 0.0;
 				*cmdGear = -1;
 				*cmdAcc = 1;
-				if(ass == 0 && fabs(parkdist)>0.01 && fabs(angle)>0.01)*cmdAcc = 0.6;
+				if (ass == 0 && fabs(parkdist) > 0.01 && fabs(angle) > 0.01)*cmdAcc = 0.6;
 			}
 		}
 		else if (TurnRight) {
 			*cmdGear = 1;
 			if (!backcar && !FirstStop) {
 				*cmdSteer = -0.5*fabs(atan2(haltX - _carX, haltY - _carY));
-				if (_speed<midlined) *cmdAcc = 0.2, *cmdBrake = 0;
+				if (_speed < midlined) *cmdAcc = 0.2, *cmdBrake = 0;
 				else *cmdAcc = 0, *cmdBrake = 0.2;
 			}
-			if (!backcar && (FirstStop||fabs(angle)<0.3)) {
+			if (!backcar && (FirstStop || fabs(angle) < 0.3)) {
 				float k1 = 1.0, k2 = 0.2, k3 = 0.04;
 				if (fabs(_lotX - X1) < 1 && fabs(_lotY - Y1) < 1) k2 = 0.1; //#1
 				if (fabs(_lotX - X2) < 1 && fabs(_lotY - Y2) < 1) k1 = 1.0201; //#2 
@@ -197,7 +209,7 @@ static void userDriverSetParam(bool* bFinished, float* cmdAcc, float* cmdBrake, 
 				if (fabs(_lotX - X4) < 1 && fabs(_lotY - Y4) < 1) k1 = 0.2;  //#4
 				if (fabs(_lotX - X5) < 1 && fabs(_lotY - Y5) < 1) k1 = 1.05; //#5
 				*cmdSteer = (k1*_caryaw - (_lotAngle + 0.61)) / 3.14;
-				*cmdBrake = k2*_speed + k3*midlined + 0.2;
+				*cmdBrake = k2 * _speed + k3 * midlined + 0.2;
 				*cmdAcc = 0;
 
 				if (_speed < 1) backcar = true;
@@ -207,20 +219,25 @@ static void userDriverSetParam(bool* bFinished, float* cmdAcc, float* cmdBrake, 
 			*cmdAcc = 0.2;
 			float k1 = 1, k2 = 0.0, k3 = 4;
 			*cmdSteer = (-k3 * atan2(_midline[20][0] - _width * k2 - 2.5, _midline[20][1])) / 3.14;
-			if (fabs(_lotX - X1) < 1 && fabs(_lotY - Y1) < 1) {k1 = 1;
+			if (fabs(_lotX - X1) < 1 && fabs(_lotY - Y1) < 1) {
+				k1 = 1;
 				*cmdSteer = (-k3 * atan2(_midline[20][0] - _width * k2 - 3.5, _midline[20][1])) / 3.14;
 			}
-			if (fabs(_lotX - X2) < 1 && fabs(_lotY - Y2) < 1) { k1 = 1.1, k2 = 0.28; 
-				*cmdSteer = ( - k3 * atan2(_midline[20][0] - _width * k2-1.5 , _midline[20][1])) / 3.14;
+			if (fabs(_lotX - X2) < 1 && fabs(_lotY - Y2) < 1) {
+				k1 = 1.1, k2 = 0.28;
+				*cmdSteer = (-k3 * atan2(_midline[20][0] - _width * k2 - 1.5, _midline[20][1])) / 3.14;
 			}
-			if (fabs(_lotX - X3) < 1 && fabs(_lotY - Y3) < 1)  {k2 = 0.002;
-				*cmdSteer = ( - k3 * atan2(_midline[20][0] - _width * k2 -2.3, _midline[20][1])) / 3.14;
+			if (fabs(_lotX - X3) < 1 && fabs(_lotY - Y3) < 1) {
+				k2 = 0.002;
+				*cmdSteer = (-k3 * atan2(_midline[20][0] - _width * k2 - 2.3, _midline[20][1])) / 3.14;
 			}
-			if (fabs(_lotX - X4) < 1 && fabs(_lotY - Y4) < 1) {k1 = 1.1, k2 = 0.28;
-				*cmdSteer = ( - k3 * atan2(_midline[20][0] - _width * k2-1.779, _midline[20][1])) / 3.14;
+			if (fabs(_lotX - X4) < 1 && fabs(_lotY - Y4) < 1) {
+				k1 = 1.1, k2 = 0.28;
+				*cmdSteer = (-k3 * atan2(_midline[20][0] - _width * k2 - 1.779, _midline[20][1])) / 3.14;
 			}
-			if (fabs(_lotX - X5) < 1 && fabs(_lotY - Y5) < 1) {k1 = 0.95, k3 = 4;
-				*cmdSteer = ( - k3 * atan2(_midline[20][0] - _width * k2-2.3, _midline[20][1])) / 3.14;
+			if (fabs(_lotX - X5) < 1 && fabs(_lotY - Y5) < 1) {
+				k1 = 0.95, k3 = 4;
+				*cmdSteer = (-k3 * atan2(_midline[20][0] - _width * k2 - 2.3, _midline[20][1])) / 3.14;
 				*cmdAcc = 0.18;
 			}
 			*cmdGear = 2;
@@ -229,11 +246,11 @@ static void userDriverSetParam(bool* bFinished, float* cmdAcc, float* cmdBrake, 
 		else {
 			*cmdAcc = 1;
 			*cmdBrake = 0;
-			*cmdSteer = (_yaw - 8 * atan2(_midline[10][0]-1.5, _midline[10][1])) / 3.14;
+			*cmdSteer = (_yaw - 8 * atan2(_midline[10][0] - 1.5, _midline[10][1])) / 3.14;
 			*cmdGear = 1;
 		}
 	}
-	if (*bFinished) { 
+	if (*bFinished) {
 		float k1 = 1.6;
 		if (fabs(_lotX - X3) < 1 && fabs(_lotY - Y3) < 1)  k1 = 2; //#3
 		if (fabs(_lotX - X4) < 1 && fabs(_lotY - Y4) < 1)  k1 = 2; //#4
@@ -291,7 +308,7 @@ static void userDriverSetParam(bool* bFinished, float* cmdAcc, float* cmdBrake, 
 	//if(*bFinished)printf("\n============bFinished============\n");
 
 	printf("yaw:%.1f ", _yaw);
-	
+
 	printf("\n");
 	///=======================================printf functions============================================
 
